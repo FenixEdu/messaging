0dab4a97017cc65183da070fff401ede9b9fd831
Gil Lacerda <gilclacerda@gmail.com>
2017-04-13 20:09:04 +0100

    Allow senders to send messages to ad hoc recipients Allow locking message interface to a specific sender
    Recipients to be used in the message sending interface are now signed by the server using JWT.
    Through these server provided recipient tokens it is now possile for a sender to send a message
    to any recipient group. Use MessageBean::addAdHocRecipient to provide these tokens and
    MessageBean::selectRecipient to pre-select a recipient in the interface. Direct usage of the
    bean's setters is discouraged.
    To prevent the current user of switching senders in situations where it does not make sense,
    such as in the workflow of a role-specific sender task, it is now possible to lock the
    interface to a pre-selected sender by using MessageBean::setLockedSender.

--- messaging-core/src/main/java/org/fenixedu/bennu/MessagingConfiguration.java
+++ messaging-core/src/main/java/org/fenixedu/bennu/MessagingConfiguration.java
@@ -1,8 +1,27 @@
 package org.fenixedu.bennu;
 
 import org.fenixedu.bennu.spring.BennuSpringModule;
+import org.fenixedu.commons.configuration.ConfigurationInvocationHandler;
+import org.fenixedu.commons.configuration.ConfigurationManager;
+import org.fenixedu.commons.configuration.ConfigurationProperty;
 
 @BennuSpringModule(basePackages = "org.fenixedu.messaging", bundles = "MessagingResources")
 public class MessagingConfiguration {
+
+    @ConfigurationManager(description = "Email Dispatcher Configurations")
+    public interface ConfigurationProperties {
+        @ConfigurationProperty(key = "messaging.jwt.api.secret.key")
+        String jwtKey();
+
+        @ConfigurationProperty(key = "messaging.jwt.api.algorithm", defaultValue = "HS256")
+        String jwtAlgorithm();
+
+        @ConfigurationProperty(key = "messaging.jwt.api.ttl.ms", defaultValue = "1800000")
+        Long jwtTTL();
+    }
+
+    public static ConfigurationProperties getConfiguration() {
+        return ConfigurationInvocationHandler.getConfiguration(ConfigurationProperties.class);
+    }
 
 }
