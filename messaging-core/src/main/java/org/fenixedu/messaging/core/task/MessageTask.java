5db8f4ef8f94ce64c3f3da8ac05987be625c20b1
Daniel Correia <danielcorreia96@users.noreply.github.com>
2018-03-26 18:27:36 +0100

    Improve MessageTask performance FMSG-19 #resolve
    * Improve MessageTask performance FMSG-19 #resolve
    
    * Update MessageTask.java

--- messaging-core/src/main/java/org/fenixedu/messaging/core/task/MessageTask.java
+++ messaging-core/src/main/java/org/fenixedu/messaging/core/task/MessageTask.java
@@ -7,10 +7,14 @@ import org.fenixedu.messaging.core.domain.MessageDispatchReport;
 import org.fenixedu.messaging.core.domain.MessagingSystem;
 import org.fenixedu.messaging.core.domain.Sender;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Task(englishTitle = "Message Task", readOnly = true)
 public class MessageTask extends CronTask {
     @Override
     public void runTask() {
+        Set<Sender> sendersFromMessages = new HashSet<>();
         int dispatched = 0;
         for (final Message message : MessagingSystem.getPendingMessages()) {
             long start = System.currentTimeMillis();
@@ -19,14 +23,16 @@ public class MessageTask extends CronTask {
                 getLogger().info("Dispatched message: {} in {}ms for {} addresses", message.getExternalId(), System
                         .currentTimeMillis() - start, report.getTotalCount());
                 dispatched++;
+                sendersFromMessages.add(message.getSender());
             }
         }
         if (dispatched > 0) {
             taskLog("Dispatched %d messages\n", dispatched);
         }
 
+        taskLog("%d possible senders to prune%n", sendersFromMessages.size());
         int pruned = 0;
-        for (Sender sender : Sender.all()) {
+        for (Sender sender : sendersFromMessages){
             int diff = sender.getMessageSet().size();
             sender.pruneMessages();
             diff = diff - sender.getMessageSet().size();
