f63e6f4ac10cb03ce6f05e6a44443320e1f83ff8
Daniel Correia <danielcorreia96@users.noreply.github.com>
2018-04-02 18:34:49 +0100

    MessagingUtils redirect doesn't use context FMSG-23 #resolve

--- messaging-core/src/main/java/org/fenixedu/messaging/core/ui/MessagingUtils.java
+++ messaging-core/src/main/java/org/fenixedu/messaging/core/ui/MessagingUtils.java
@@ -5,22 +5,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
-public class MessagingUtils {
+public final class MessagingUtils {
+    private static final String MESSAGING_MESSAGE_BEAN = "MESSAGING_MESSAGE_BEAN";
 
-    public static final String MESSAGING_MESSAGE_BEAN = "MESSAGING_MESSAGE_BEAN";
+    private MessagingUtils(){
+        // Utility classes should have a private constructor to prevent instantiation.
+    }
 
-    public static <T> T redirectToNewMessage(HttpServletRequest request, HttpServletResponse response, MessageBean messageBean)
+    public static <T> T redirectToNewMessage(final HttpServletRequest request, final HttpServletResponse response, final MessageBean messageBean)
             throws IOException {
         request.getSession().setAttribute(MESSAGING_MESSAGE_BEAN, messageBean);
-        response.sendRedirect("/messaging/message");
+        response.sendRedirect(request.getContextPath() + "/messaging/message");
         return null;
     }
 
-    public static Optional<MessageBean> getMessageBeanFromSession(HttpServletRequest request) {
+    public static Optional<MessageBean> getMessageBeanFromSession(final HttpServletRequest request) {
         return Optional.ofNullable((MessageBean) request.getSession().getAttribute(MESSAGING_MESSAGE_BEAN));
     }
 
-    public static void clearMessageBeanFromSession(HttpServletRequest request) {
+    public static void clearMessageBeanFromSession(final HttpServletRequest request) {
         request.getSession().removeAttribute(MessagingUtils.MESSAGING_MESSAGE_BEAN);
     }
 }
