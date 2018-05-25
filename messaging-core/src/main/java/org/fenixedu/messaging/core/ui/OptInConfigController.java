1ce040a8d836f6caab343da0ce5101c28588ed5e
Daniel Correia <dduarte96@hotmail.com>
2018-05-25 22:25:14 +0100

    Update OptedOutGroup using OptInConfig interface FMSG-42 #resolve

--- messaging-core/src/main/java/org/fenixedu/messaging/core/ui/OptInConfigController.java
+++ messaging-core/src/main/java/org/fenixedu/messaging/core/ui/OptInConfigController.java
@@ -5,10 +5,12 @@ import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.core.signals.Signal;
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
+import org.fenixedu.messaging.core.domain.MessagingSystem;
 import org.fenixedu.messaging.core.domain.Sender;
 import org.fenixedu.messaging.core.domain.Sender_Base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,28 +38,50 @@ public class OptInConfigController {
                 .filter(sender -> sender.getRecipients().stream().anyMatch(group -> group.isMember(Authenticate.getUser())))
                 .forEach(sender -> optInRequiredSenders.put(sender, sender.getOptedInUsers().contains(Authenticate.getUser())));
 
+        final boolean canConfigOptOut = MessagingSystem.getInstance().getOptOutAvailable().isMember(Authenticate.getUser()) ||
+                MessagingSystem.getInstance().getOptedOutGroup().isMember(Authenticate.getUser());
+        
+        model.addAttribute("canConfigOptOut", canConfigOptOut);
+        if (canConfigOptOut) {
+            model.addAttribute("isOptedOut", MessagingSystem.getInstance().getOptedOutGroup().isMember(Authenticate.getUser()));
+        }
+
         model.addAttribute("optInRequiredSenders", optInRequiredSenders);
         return "/messaging/listOptIns";
     }
 
     @RequestMapping(value = {"/optIn/{sender}"})
-    public String optIn(@PathVariable Sender sender){
+    public ResponseEntity optIn(@PathVariable Sender sender){
         final User user = Authenticate.getUser();
         FenixFramework.atomic(() -> {
             sender.addOptedInUser(user);
             Signal.emit(MessagingConfiguration.OPTIN_STATUS_UPDATE, new OptInUpdateEvent(sender, user, true));
         });
-        return "redirect:/messaging/subscriptions/senders";
+        return ResponseEntity.ok().build();
     }
 
     @RequestMapping(value = {"/optOut/{sender}"})
-    public String optOut(@PathVariable Sender sender){
+    public ResponseEntity optOut(@PathVariable Sender sender){
         final User user = Authenticate.getUser();
         FenixFramework.atomic(() -> {
             sender.removeOptedInUser(user);
             Signal.emit(MessagingConfiguration.OPTIN_STATUS_UPDATE, new OptInUpdateEvent(sender, user, false));
         });
-        return "redirect:/messaging/subscriptions/senders";
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = {"/globalOptIn"})
+    public ResponseEntity globalOptIn(){
+        final User user = Authenticate.getUser();
+        FenixFramework.atomic(() -> MessagingSystem.getInstance().optIn(user));
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = {"/globalOptOut"})
+    public ResponseEntity globalOptOut(){
+        final User user = Authenticate.getUser();
+        FenixFramework.atomic(() -> MessagingSystem.getInstance().optOut(user));
+        return ResponseEntity.ok().build();
     }
 
 }
